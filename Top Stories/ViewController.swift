a464704a00729b92168dc7868c953caeb15e86d2
Alex <alexandrakazaglis@gmail.com>
2018-11-24 13:50:37 -0600

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by Alexandra Kazaglis on 11/24/18.
+//  Copyright © 2018 Alexandra Kazaglis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
